5bb91e2407a12d86332a8acd53ae4582acd29e34
hxl <hel13140302@126.com>
2018-03-02 10:03:52 +0800

    Add entrance file

---
+++ App.js
@@ -0,0 +1,14 @@
+/**
+ * CreateTime: 2018-03-01 19:28
+ * Author:      XiaoLiang He
+ * Description: entrance
+ * Web:         www.hxl2lgy.top
+ * Email:       hel13140302@126.com
+ */
+function App()
+{
+
+}
+let proto = App.prototype;
+
+module.exports = App;
